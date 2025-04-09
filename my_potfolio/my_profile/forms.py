adc2a2a921f4631c064f7fdf241cf58089272620
Tschuddy <141195843+Tschuddy@users.noreply.github.com>
2025-04-10 00:39:15 +0100

    Delete my_potfolio directory
    it was bad

--- my_potfolio/my_profile/forms.py
+++
@@ -1,8 +0,0 @@
-# # forms.py
-# from django import forms
-
-# class ContactForm(forms.Form):
-#     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'name-field'}))
-#     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'email-field'}))
-#     subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'subject-field'}))
-#     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'id': 'message-field', 'rows': 10}))
--- my_potfolio/staticfiles/Readme.txt
+++
@@ -1,6 +0,0 @@
-Thanks for downloading this template!
-
-Template Name: my_potfolio
-Template URL: https://github.com/Tschuddy/
-Author: Tschuddy
-License: https://github.com/Tschuddy/
